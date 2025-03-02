1a01106a33d210afdd3c714ab7c59bb8b2e62417
GwangCheonLee <raitmid56@gmail.com>
2025-03-02 15:56:53 +0900

    feat: add delete functionality to goal item

--- App.js
+++ App.js
@@ -13,6 +13,13 @@ export default function App() {
     ]);
   }
 
+  const deleteGoalHandler = (id) => {
+    setCourseGoals((currentCourseGoals) => {
+        return currentCourseGoals.filter((goal) => goal.id !== id);
+      },
+    );
+  };
+
   return (
     <View style={styles.appContainer}>
       <GoalInput onAddGoal={addGoalHandler} />
@@ -24,7 +31,7 @@ export default function App() {
             /*Text Component 는 과거 IOS 에서는 borderRadius가 적용되지 않아 View Component로 감싸서 적용했었음
             현재 expo 52.0.0 에서는 borderRadius가 적용되는 것을 확인*/
             return (
-              <GoalItem text={itemData.item.text} />
+              <GoalItem id={itemData.item.id} text={itemData.item.text} onDeleteItem={deleteGoalHandler} />
             );
           }}
           // FlatList 는 기본적으로 배열의 Key 값이 있다면 사용하게 되는데 API 에서 데이터를 호출하는 경우와 같이 Key 값이 없을 경우 사용
--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,9 +1,13 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
 
 function GoalItem(props) {
   return (
+    // Native APP 에서 터치를 감지할 수 있게 Pressable Component 추가
     <View style={styles.goalItem}>
-      <Text style={styles.goalText}>{props.text}</Text>
+      <Pressable android_ripple={{color: '#dddddd'}} onPress={props.onDeleteItem.bind(this, props.id)}
+                 style={({pressed}) => pressed && styles.pressedItem}>
+        <Text style={styles.goalText}>{props.text}</Text>
+      </Pressable>
     </View>
   );
 }
@@ -11,13 +15,18 @@ function GoalItem(props) {
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: '#5E0ACC',
   },
 
+  pressedItem: {
+    opacity: 0.5,
+  },
+
   goalText: {
     color: '#FFFFFF',
+    padding: 8,
+
   },
 });
 
