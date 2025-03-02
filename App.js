e8ee988d5db50ab568f0c48d97767b61fab3aaf1
GwangCheonLee <raitmid56@gmail.com>
2025-03-02 17:15:55 +0900

    feat: add image to goal input modal and update styles

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import {Button, FlatList, StyleSheet, View} from 'react-native';
 import {useState} from 'react';
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
+import {StatusBar} from 'expo-status-bar';
 
 export default function App() {
   const [modalIsVisible, setModalIsVisible] = useState(false);
@@ -31,27 +32,31 @@ export default function App() {
   };
 
   return (
-    <View style={styles.appContainer}>
-      <Button title={'Add New Goal'} color="#5E0ACC" onPress={startAddGoalHandler} />
-      {modalIsVisible && <GoalInput onAddGoal={addGoalHandler} onCancel={endAddGoalHandler} visible={modalIsVisible} />}
-      <View style={styles.goalsContainer}>
-        <FlatList
-          alwaysBounceVertical={false}
-          data={courseGoals}
-          renderItem={(itemData) => {
-            /*Text Component 는 과거 IOS 에서는 borderRadius가 적용되지 않아 View Component로 감싸서 적용했었음
-            현재 expo 52.0.0 에서는 borderRadius가 적용되는 것을 확인*/
-            return (
-              <GoalItem id={itemData.item.id} text={itemData.item.text} onDeleteItem={deleteGoalHandler} />
-            );
-          }}
-          // FlatList 는 기본적으로 배열의 Key 값이 있다면 사용하게 되는데 API 에서 데이터를 호출하는 경우와 같이 Key 값이 없을 경우 사용
-          keyExtractor={(item) => {
-            return item.id;
-          }}
-        ></FlatList>
+    <>
+      <StatusBar style="light" />
+      <View style={styles.appContainer}>
+        <Button title={'Add New Goal'} color="#a065ec" onPress={startAddGoalHandler} />
+        {modalIsVisible &&
+          <GoalInput onAddGoal={addGoalHandler} onCancel={endAddGoalHandler} visible={modalIsVisible} />}
+        <View style={styles.goalsContainer}>
+          <FlatList
+            alwaysBounceVertical={false}
+            data={courseGoals}
+            renderItem={(itemData) => {
+              /*Text Component 는 과거 IOS 에서는 borderRadius가 적용되지 않아 View Component로 감싸서 적용했었음
+              현재 expo 52.0.0 에서는 borderRadius가 적용되는 것을 확인*/
+              return (
+                <GoalItem id={itemData.item.id} text={itemData.item.text} onDeleteItem={deleteGoalHandler} />
+              );
+            }}
+            // FlatList 는 기본적으로 배열의 Key 값이 있다면 사용하게 되는데 API 에서 데이터를 호출하는 경우와 같이 Key 값이 없을 경우 사용
+            keyExtractor={(item) => {
+              return item.id;
+            }}
+          ></FlatList>
+        </View>
       </View>
-    </View>
+    </>
   );
 }
 
@@ -60,6 +65,7 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 50,
     paddingHorizontal: 16,
+    backgroundColor: '#1e085a',
   },
   goalsContainer: {
     flex: 5,
