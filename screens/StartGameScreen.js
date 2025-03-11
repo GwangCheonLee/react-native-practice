2c52b84526869cf2735ec074ec833b274a2bc525
GwangCheonLee <raitmid56@gmail.com>
2025-03-11 22:35:24 +0900

    feat: implement StartGameScreen and add PrimaryButton component

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,12 @@
-const StartGameScreen = () => {
+import {TextInput, View} from 'react-native';
+import PrimaryButton from '../components/PrimaryButton';
 
+const StartGameScreen = () => {
+  return <View>
+    <TextInput />
+    <PrimaryButton>Reset</PrimaryButton>
+    <PrimaryButton>Confirm</PrimaryButton>
+  </View>;
 };
 
 export default StartGameScreen;
